ddaae64ae9ff7b0c490cf4075d7f45e5b5302ef8
huckclem <65532111+huckclem@users.noreply.github.com>
2020-05-28 17:04:13 +0200

    Update ITS2_first_insight.R
    Name of variables and graphes were changed

--- sequences_processing/ITS2/ITS2_dada2_output_jrl2020/ITS2_first_insight.R
+++ sequences_processing/ITS2/ITS2_dada2_output_jrl2020/ITS2_first_insight.R
@@ -1,48 +1,49 @@
+# Loading of the packages
+
 library(readr)
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
 
+# Importing the basic statistics data from DADA2 ITS2 process and change name of variables
+
 (data_frame <- read_tsv("ITS2_stats_jrl2020.tsv") %>%
     slice(-c(1)) %>%
     rename(c( "ID"= "sample-id")) %>%
-    rename(c("non_chimeric" = "non-chimeric")) %>%
-    rename(c("pct_of_non_chimeric" = "percentage of input non-chimeric")) %>%
-    rename(c("filtered_pct" = "percentage of input passed filter"))
+    rename(c("nb_non_chimeric_reads" = "non-chimeric")) %>%
+    rename(c("pct_input_non_chimeric" = "percentage of input non-chimeric")) %>%
+    rename(c("pct_filtered_reads" = "percentage of input passed filter"))
 )
 
-data_frame$filtered_pct <- as.numeric(data_frame$filtered_pct)
-data_frame$non_chimeric <- as.numeric(data_frame$non_chimeric)
+# convert character variables into numeric variables
 
-arrange(data_frame, filtered_pct)
+data_frame$pct_filtered_reads <- as.numeric(data_frame$pct_filtered_reads)
+data_frame$pct_input_non_chimeric <- as.numeric(data_frame$pct_input_non_chimeric)
+data_frame$nb_non_chimeric_reads <- as.numeric(data_frame$nb_non_chimeric_reads)
 
-ggplot(data=data_frame) + geom_col(mapping = aes(x=ID, y = non_chimeric, fill = ID))
+# Plot a first graph to see the number of read for each replicate
+ggplot(data=data_frame) + geom_col(mapping = aes(x=ID, y = pct_input_non_chimeric, fill = ID))
+
 
 # remove the Mock, ExtC and PCR lines
-
 (no_test_df <- filter(data_frame, grepl("*-*-", ID)))
 
-# Group the lines given the sample they come from
+# plot a second graph without the three test samples (mock, PCR and EXternal contaminants)
+ggplot(data=no_test_df) + geom_col(mapping = aes(x=ID, y = pct_input_non_chimeric, fill = ID))
 
-(no_test_df <- separate(data = no_test_df, col = "ID", sep = 3, into = c("sample_number", "primer_number"), remove = FALSE) %>% 
+
+# Separate the ID column so that we see the replicate number and the sample number of each sample ID
+
+(no_test_df <- separate(data = no_test_df, col = "ID", sep = 3, into = c("sample_number", "replicate_number"), remove = FALSE) %>% 
     group_by(sample_number) %>%
-    mutate(prop_reads_each_primer = (non_chimeric/sum(non_chimeric))*100) %>%
-    select(ID, non_chimeric, prop_reads_each_primer, everything())
-)
+    summarise(total_reads = sum(nb_non_chimeric_reads)))# Here we have the total_reads for each sample
 
-(no_test_df <- group_by(no_test_df, sample_number)) %>%
-  summarise(total_reads = sum(non_chimeric)) %>%
-  ggplot(mapping = aes(x = reorder(sample_number,total_reads, FUN = median), y = total_reads)) + geom_point()
-)
-
-filter(no_test_df, prop_reads_each_primer < 5)
+# This graph is a bonus : we can see wich sample have the lowest/higher number of reads
+  ggplot(data = no_test_df, mapping = aes(x = reorder(sample_number,total_reads, FUN = median), y = total_reads)) + geom_point()
 
 # I think we should remove B-2-3, B-5-1, B-7-3 and B-9-3 from the dataset beacause they
 # have few reads
 
-
-ggplot(data = no_test_df) + geom_boxplot(mapping=aes(x = primer_number, y = prop_reads_each_primer))
-ggplot(data = no_test_df) + geom_col(mapping = aes(x = ID, y = prop_reads_each_primer))
 
   
 
