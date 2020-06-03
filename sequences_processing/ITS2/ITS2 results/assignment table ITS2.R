f78ad9c143b8f9faac722cbaa0f8ccc4684af1aa
huckclem <65532111+huckclem@users.noreply.github.com>
2020-06-03 16:15:55 +0200

    Update assignment table ITS2.R
    Split the taxonomic columns in kingdom,  phylum...

--- sequences_processing/ITS2/ITS2 results/assignment table ITS2.R
+++ sequences_processing/ITS2/ITS2 results/assignment table ITS2.R
@@ -24,6 +24,15 @@ assignment_table <- rename(assignment_table, "B_2_3"="B-2-3")
 
 (assignment_table <- select(assignment_table, -Feature_ID, -Confidence, -B_2_3, -ExtCont, -MockCom))
 
+taxa_assignment_table <- separate(data = assignment_table, col = Taxon, sep = "([pcofgs]__)", into = c("Kingdom","Phylum","Class","Order","Family","Gender","Species"))
+taxa_assignment_table$Phylum <-  gsub(";","",taxa_assignment_table$Phylum)
+taxa_assignment_table$Class <-  gsub(";","",taxa_assignment_table$Class)
+taxa_assignment_table$Order <-  gsub(";","",taxa_assignment_table$Order)
+taxa_assignment_table$Family <-  gsub(";","",taxa_assignment_table$Family)
+taxa_assignment_table$Gender <-  gsub(";","",taxa_assignment_table$Gender)
+taxa_assignment_table$Kingdom <-  gsub(";","",taxa_assignment_table$Kingdom)
+taxa_assignment_table$Kingdom <-  gsub("k__","",taxa_assignment_table$Kingdom)
+
 # To do the rarefaction curve it's needed to switch the columns and lines from the table
 
 (t_assignment_table <- data.frame(t(assignment_table[-1])))
@@ -38,6 +47,6 @@ raremax <- min(rowSums(t_assignment_table))
 Srare <- rarefy(t_assignment_table, raremax)
 plot(S, Srare, xlab = "Observed No. of Species", ylab = "Rarefied No. of Species")
 abline(0,1)
-rarecurve(t_assignment_table, step = 20, sample = raremax, col = "blue", cex = 0.6)
+rarecurve(t_assignment_table, step = 20, sample = raremax, col = "blue", cex = 0.6, ylab = "OTU number")
 
 
